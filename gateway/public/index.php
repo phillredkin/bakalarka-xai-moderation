c1a17ff4085a23e22172fa8ba2c5afb0b67efee0
Fylyp Redkin <phill.redkin2005@gmail.com>
2026-03-28 14:31:57 +0100

    Allow cross-origin requests from Vercel frontend

--- gateway/public/index.php
+++ gateway/public/index.php
@@ -1,6 +1,10 @@
 <?php
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: https://clarimod.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Max-Age: 86400');
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 

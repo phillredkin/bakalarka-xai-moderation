57e7dfc5f21f13564bfa39a9932e4d9a73a97225
Fylyp Redkin <phill.redkin2005@gmail.com>
2026-03-28 14:37:37 +0100

    Handle CORS preflight requests

--- gateway/public/index.php
+++ gateway/public/index.php
@@ -6,6 +6,11 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Max-Age: 86400');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($path === '/' || $path === '') {
